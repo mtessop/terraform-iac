c04a65e0fb6ecbaec59af3f68400fd65f2de5e09
mtessop <mtessop@gmail.com>
2025-02-26 12:22:03 -0600

    update

--- S3.tf
+++ S3.tf
@@ -1,21 +1,16 @@
-data "aws_s3_bucket" "existing_bucket" {
+data "aws_s3_bucket" "existing" {
   bucket = "my-terraform-storage-bucket"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "my-terraform-storage-bucket"
-  
-  # Only create if the bucket does not exist
-  count = length(data.aws_s3_bucket.existing_bucket.id) > 0 ? 0 : 1
 
-  force_destroy = true
-}
+  lifecycle {
+    prevent_destroy = true
+  }
 
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  count  = length(data.aws_s3_bucket.existing_bucket.id) > 0 ? 0 : 1
-
-  versioning_configuration {
-    status = "Enabled"
+  tags = {
+    Name        = "MyTerraformS3Bucket"
+    Environment = "Dev"
   }
 }
--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-storage-bucket"
-    key            = "terraform/state"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock"
   }
 }
