9be18aa8f106a7a7ee9de2d6406187ee2f12904c
mtessop <mtessop@gmail.com>
2025-02-26 12:25:52 -0600

    update

--- S3.tf
+++ S3.tf
@@ -1,9 +1,12 @@
+# Check if the bucket already exists
 data "aws_s3_bucket" "existing" {
   bucket = "my-terraform-storage-bucket"
 }
 
+# Create the bucket only if it does not exist
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "my-terraform-storage-bucket"
+  count  = length(data.aws_s3_bucket.existing.id) > 0 ? 0 : 1
 
   lifecycle {
     prevent_destroy = true
